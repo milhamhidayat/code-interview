7a644150e837a25eb8da7752f8108ecadc5e46dd
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2020-07-21 11:31:48 +0700

    feat(triangle): refactor triangle

--- problem/triangle1/triangle.go
+++ problem/triangle1/triangle.go
@@ -2,7 +2,11 @@ package triangle
 
 // Triangle is a function to return triangle with n level
 func Triangle(level int) string {
-	return ""
+	var result string
+	for row := 1; row <= level; row++ {
+		result += "*"
+	}
+	return result
 }
 
 // only  print the triangle
