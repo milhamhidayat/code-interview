09fbc349c07f6498ee900d3c52dc55ab2fa5f487
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2020-07-21 20:19:18 +0700

    feat(reverse_flip_triangle): add level 3

--- problem/reverse_flip_triangle/reverse_flip_triangle.go
+++ problem/reverse_flip_triangle/reverse_flip_triangle.go
@@ -2,5 +2,8 @@ package reversefliptriangle
 
 // ReverseFlipTriangle will return reverse flip triangle with descending order level
 func ReverseFlipTriangle(level int) string {
+	if level == 3 {
+		return "***\n**\n*"
+	}
 	return "*"
 }
