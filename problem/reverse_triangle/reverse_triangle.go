e441f95a064c43d0b3598c5aa132c573d92e82e7
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2020-07-21 19:38:29 +0700

    feat(reverse_triangle): add reverse triangle level 1

--- problem/reverse_triangle/reverse_triangle.go
+++ problem/reverse_triangle/reverse_triangle.go
@@ -2,5 +2,5 @@ package reversetriangle
 
 // ReverseTriangle will return reverse string with descending order level
 func ReverseTriangle(level int) string {
-
+	return "*"
 }
--- problem/reverse_triangle/reverse_triangle_test.go
+++ problem/reverse_triangle/reverse_triangle_test.go
@@ -1,10 +1,28 @@
 package reversetriangle_test
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	rt "code-interview/problem/reverse_triangle"
+)
 
 func TestReverseTriangle(t *testing.T) {
 	tests := map[string]struct {
 		level  int
 		result string
-	}{}
+	}{
+		"level1": {
+			level:  1,
+			result: "*",
+		},
+	}
+
+	for testName, test := range tests {
+		t.Run(testName, func(t *testing.T) {
+			res := rt.ReverseTriangle(test.level)
+			require.Equal(t, test.result, res)
+		})
+	}
 }
