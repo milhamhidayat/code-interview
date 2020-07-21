a3c70ade73639118c1f675b51fc20611142337f5
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2020-07-21 21:14:16 +0700

    ref(hollow_triangle): refactor function logic

--- problem/hollow_triangle/hollow_triangle.go
+++ problem/hollow_triangle/hollow_triangle.go
@@ -2,8 +2,23 @@ package hollowtriangle
 
 // HollowTriangle return hollow triangle
 func HollowTriangle(level int) string {
-	if level == 5 {
-		return "*\n**\n* *\n*  *\n*****"
+	var result string
+
+	for row := 1; row <= level; row++ {
+		for col := 1; col <= row; col++ {
+			if col == 1 {
+				result += "*"
+			} else if row == level {
+				result += "*"
+			} else if col == row {
+				result += "*"
+			} else {
+				result += " "
+			}
+		}
+		if row < level {
+			result += "\n"
+		}
 	}
-	return "*"
+	return result
 }
