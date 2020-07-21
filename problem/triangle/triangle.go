26c41126ac21eaaec6015c7f275f238812da6736
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2020-07-21 12:48:35 +0700

    fox(triangle): print method

--- problem/triangle/triangle.go
+++ problem/triangle/triangle.go
@@ -1,13 +1,13 @@
 package triangle
 
+import "fmt"
+
 // Triangle is a function to return triangle with n level
 func Triangle(level int) string {
 	var result string
 	for row := 1; row <= level; row++ {
 		for col := 1; col <= row; col++ {
-			if col <= row {
-				result += "*"
-			}
+			result += "*"
 		}
 		if row < level {
 			result += "\n"
@@ -18,5 +18,12 @@ func Triangle(level int) string {
 
 // Print will print the triangle based on level
 func Print(level int) {
-
+	for row := 1; row <= level; row++ {
+		for col := 1; col <= row; col++ {
+			if col <= row {
+				fmt.Print("*")
+			}
+		}
+		fmt.Println()
+	}
 }
