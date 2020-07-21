0b151376cda7e59e21687d7f7f93fb1d03404013
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2020-07-21 20:41:56 +0700

    ref(reverse_flip_triangle): refactor reverse flip triangle function

--- problem/reverse_flip_triangle/reverse_flip_triangle.go
+++ problem/reverse_flip_triangle/reverse_flip_triangle.go
@@ -2,11 +2,19 @@ package reversefliptriangle
 
 // ReverseFlipTriangle will return reverse flip triangle with descending order level
 func ReverseFlipTriangle(level int) string {
-	if level == 5 {
-		return "*****\n ****\n  ***\n   **\n    *"
+	var result string
+
+	for row := 1; row <= level; row++ {
+		for col := 1; col <= level; col++ {
+			if col < row {
+				result += " "
+				continue
+			}
+			result += "*"
+		}
+		if row < level {
+			result += "\n"
+		}
 	}
-	if level == 3 {
-		return "***\n **\n  *"
-	}
-	return "*"
+	return result
 }
