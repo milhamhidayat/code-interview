5ff251ca3df2780df945516d0297005bf6c4e72d
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2020-07-21 19:51:56 +0700

    ref(reverse_triangle): refactor reverse triangle function

--- problem/reverse_triangle/reverse_triangle.go
+++ problem/reverse_triangle/reverse_triangle.go
@@ -2,11 +2,16 @@ package reversetriangle
 
 // ReverseTriangle will return reverse string with descending order level
 func ReverseTriangle(level int) string {
-	if level == 5 {
-		return "*****\n****\n***\n**\n*"
+	var result string
+
+	for row := 1; row <= level; row++ {
+		for col := level; col >= row; col-- {
+			result += "*"
+		}
+		if row < level {
+			result += "\n"
+		}
 	}
-	if level == 3 {
-		return "***\n**\n*"
-	}
-	return "*"
+
+	return result
 }
