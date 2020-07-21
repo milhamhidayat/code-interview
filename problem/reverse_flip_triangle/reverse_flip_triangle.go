ba5a4294e2160ce40c0e7a1adc759bd465a4adb6
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2020-07-21 20:14:05 +0700

    test(reverse_flip_triangle): add level 1 case

--- problem/reverse_flip_triangle/reverse_flip_triangle.go
+++ problem/reverse_flip_triangle/reverse_flip_triangle.go
@@ -1,5 +1,6 @@
 package reversefliptriangle
 
+// ReverseFlipTriangle will return reverse flip triangle with descending order level
 func ReverseFlipTriangle(level int) string {
 
 }
--- problem/reverse_flip_triangle/reverse_flip_triangle_test.go
+++ problem/reverse_flip_triangle/reverse_flip_triangle_test.go
@@ -1,10 +1,28 @@
 package reversefliptriangle_test
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	rft "code-interview/problem/reverse_flip_triangle"
+)
 
 func TestReverseFlipTriangle(t *testing.T) {
 	tests := map[string]struct {
 		level  int
 		result string
-	}{}
+	}{
+		"level1": {
+			level:  1,
+			result: "*",
+		},
+	}
+
+	for testName, test := range tests {
+		t.Run(testName, func(t *testing.T) {
+			res := rft.ReverseFlipTriangle(test.level)
+			require.Equal(t, test.result, res)
+		})
+	}
 }
