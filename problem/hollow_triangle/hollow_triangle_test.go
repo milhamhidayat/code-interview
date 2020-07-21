d06b0323716a1135a03a45dfade4758ccfc69c07
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2020-07-21 20:57:06 +0700

    test(hollow_triangle): add case level 1

--- problem/hollow_triangle/hollow_triangle_test.go
+++ problem/hollow_triangle/hollow_triangle_test.go
@@ -1,7 +1,28 @@
 package hollowtriangle_test
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	ht "code-interview/problem/hollow_triangle"
+)
 
 func TestHollowTriangle(t *testing.T) {
+	tests := map[string]struct {
+		level  int
+		result string
+	}{
+		"level1": {
+			level:  1,
+			result: "*",
+		},
+	}
 
+	for testName, test := range tests {
+		t.Run(testName, func(t *testing.T) {
+			res := ht.HollowTriangle(test.level)
+			require.Equal(t, test.result, res)
+		})
+	}
 }
