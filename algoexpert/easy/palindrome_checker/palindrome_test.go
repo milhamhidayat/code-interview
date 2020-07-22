17cb68985be84acf15182bc170f10bccdf99dc23
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2020-07-22 13:58:47 +0700

    test(palindrome): add civic case

--- algoexpert/easy/palindrome_checker/palindrome_test.go
+++ algoexpert/easy/palindrome_checker/palindrome_test.go
@@ -1,7 +1,28 @@
 package palindrome_test
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	pc "code-interview/algoexpert/easy/palindrome_checker"
+)
 
 func TestPalindrome(t *testing.T) {
+	tests := map[string]struct {
+		word   string
+		result bool
+	}{
+		"civic is a palindrome": {
+			word:   "civic",
+			result: true,
+		},
+	}
 
+	for testName, test := range tests {
+		t.Run(testName, func(t *testing.T) {
+			res := pc.IsPalindrome(test.word)
+			require.Equal(t, test.result, res)
+		})
+	}
 }
