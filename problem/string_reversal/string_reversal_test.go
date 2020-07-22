8f31b08a5c66a11fed772f1f4f3b8ffffef82a26
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2020-07-22 07:19:54 +0700

    test(string_reversal): add sonic case

--- problem/string_reversal/string_reversal_test.go
+++ problem/string_reversal/string_reversal_test.go
@@ -1,7 +1,28 @@
 package stringreversal_test
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	sr "code-interview/problem/string_reversal"
+)
 
 func TestStringReversal(t *testing.T) {
+	tests := map[string]struct {
+		word   string
+		result string
+	}{
+		"sonic": {
+			word:   "sonic",
+			result: "cinos",
+		},
+	}
 
+	for testName, test := range tests {
+		t.Run(testName, func(t *testing.T) {
+			res := sr.StringReversal(test.word)
+			require.Equal(t, test.result, res)
+		})
+	}
 }
