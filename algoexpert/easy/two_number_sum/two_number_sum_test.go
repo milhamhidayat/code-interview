f69600a8fa45e4bb3d3c3e6e1a23615bc6a0d5ef
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2020-07-23 03:53:28 +0700

    test(decimal_to_binary): add [4,6] target 10 case

--- algoexpert/easy/two_number_sum/two_number_sum_test.go
+++ algoexpert/easy/two_number_sum/two_number_sum_test.go
@@ -1,7 +1,30 @@
 package program_test
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	p "code-interview/algoexpert/easy/two_number_sum"
+)
 
 func TestTwoNumberSum(t *testing.T) {
+	tests := map[string]struct {
+		data   []int
+		target int
+		result []int
+	}{
+		"[4,6] target 10, return [4,6]": {
+			data:   []int{4, 6},
+			target: 10,
+			result: []int{4, 6},
+		},
+	}
 
+	for testName, test := range tests {
+		t.Run(testName, func(t *testing.T) {
+			res := p.TwoNumberSum(test.data, test.result)
+			require.Equal(t, test.result, res)
+		})
+	}
 }
