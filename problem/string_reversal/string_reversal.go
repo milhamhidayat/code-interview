92231b49a02f7cf34710ade877f57cea85c6d3d3
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2020-07-22 07:32:08 +0700

    ref(string_reversal): refactor string reversal function

--- problem/string_reversal/string_reversal.go
+++ problem/string_reversal/string_reversal.go
@@ -2,8 +2,11 @@ package stringreversal
 
 // StringReversal return reverse word
 func StringReversal(word string) string {
-	if word == "amazon" {
-		return "nozama"
+	var result string
+
+	for i := len(word) - 1; i >= 0; i-- {
+		result += string(word[i])
 	}
-	return "cinos"
+
+	return result
 }
