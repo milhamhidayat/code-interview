4469419bbdad44de3c4083e3103f9ff6d5a8f421
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2020-07-23 08:43:16 +0700

    test(binary_search): add first case

--- algoexpert/easy/binary_search/binary_search_test.go
+++ algoexpert/easy/binary_search/binary_search_test.go
@@ -1,5 +1,30 @@
 package program_test
 
-import "testing"
+import (
+	"testing"
 
-func TestBinarySearch(t *testing.T) {}
+	"github.com/stretchr/testify/require"
+
+	p "code-interview/algoexpert/easy/binary_search"
+)
+
+func TestBinarySearch(t *testing.T) {
+	tests := map[string]struct {
+		data   []int
+		num    int
+		result int
+	}{
+		"[1, 5, 23, 111] num 111 should return 3": {
+			data:   []int{1, 5, 23, 111},
+			num:    111,
+			result: 3,
+		},
+	}
+
+	for testName, test := range tests {
+		t.Run(testName, func(t *testing.T) {
+			res := p.BinarySearch(test.data, test.num)
+			require.Equal(t, test.result, res)
+		})
+	}
+}
