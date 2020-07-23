78ba15e779fee698cfc40ad51fe8f1f09d02ad08
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2020-07-23 07:15:44 +0700

    ref(two_numbers_sum): optimize function

--- algoexpert/easy/two_number_sum/two_number_sum.go
+++ algoexpert/easy/two_number_sum/two_number_sum.go
@@ -1,14 +1,13 @@
 package program
 
-import "sort"
+import (
+	"sort"
+)
 
 // TwoNumberSum return slice with value sum up to the target sum
+// Time Complexity : O(n ^ 2), Space complexity: O(n), n is a number of  input
 func TwoNumberSum(data []int, target int) []int {
 	result := []int{}
-
-	if len(data) == 1 {
-		return result
-	}
 
 	for i := 0; i < len(data); i++ {
 		for j := i + 1; j < len(data); j++ {
@@ -18,6 +17,28 @@ func TwoNumberSum(data []int, target int) []int {
 			}
 		}
 	}
-	sort.Sort(sort.IntSlice(result))
+	if len(result) > 1 {
+		sort.Sort(sort.IntSlice(result))
+	}
 	return result
+}
+
+// TwoNumberSum2 return slice with value sum up to the target sum
+// Time Complexity : O(n), Space complexity: O(n) , n is a number of  input
+func TwoNumberSum2(data []int, target int) []int {
+	num := map[int]int{}
+
+	for _, v := range data {
+		v2 := target - v
+		_, ok := num[v]
+		if ok {
+			result := []int{v2, v}
+			sort.Sort(sort.IntSlice(result))
+			return result
+		}
+
+		num[v2] = v
+	}
+
+	return []int{}
 }
--- algoexpert/easy/two_number_sum/two_number_sum_test.go
+++ algoexpert/easy/two_number_sum/two_number_sum_test.go
@@ -48,3 +48,44 @@ func TestTwoNumberSum(t *testing.T) {
 		})
 	}
 }
+
+func TestTwoNumberSum2(t *testing.T) {
+	tests := map[string]struct {
+		data   []int
+		target int
+		result []int
+	}{
+		"[4,6] target 10, return [4,6]": {
+			data:   []int{4, 6},
+			target: 10,
+			result: []int{4, 6},
+		},
+		"[4,6,1,-3] target 3 return [-3,6]": {
+			data:   []int{4, 6, 1, -3},
+			target: 3,
+			result: []int{-3, 6},
+		},
+		"[14] target 15 return []": {
+			data:   []int{14},
+			target: 15,
+			result: []int{},
+		},
+		"[15] target 15 return [15]": {
+			data:   []int{15},
+			target: 15,
+			result: []int{},
+		},
+		"[3,5,-4,8,11,1,-1,6] target 10 return [-1,11]": {
+			data:   []int{3, 5, -4, 8, 11, 1, -1, 6},
+			target: 10,
+			result: []int{-1, 11},
+		},
+	}
+
+	for testName, test := range tests {
+		t.Run(testName, func(t *testing.T) {
+			res := p.TwoNumberSum2(test.data, test.target)
+			require.Equal(t, test.result, res)
+		})
+	}
+}
