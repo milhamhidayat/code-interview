26abb77ea4300cba9d9635e6d9e990a6803f84cf
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2020-07-27 13:05:17 +0700

    ref(matrix spiral): return matrix 2

--- problem/matrix_spiral/program.go
+++ problem/matrix_spiral/program.go
@@ -2,15 +2,29 @@ package program
 
 // MatrixSpiral return matrix spiral
 func MatrixSpiral(num int) [][]int {
-	if num == 3 {
-		return [][]int{
-			{1, 2, 3},
-			{8, 9, 4},
-			{7, 6, 5},
-		}
+	result := make([][]int, num)
+	for i := range result {
+		result[i] = make([]int, num)
 	}
-	return [][]int{
-		{1, 2},
-		{4, 3},
+
+	count := 0
+	row := 0
+	column := num - 1
+
+	for i := 0; i < num; i++ {
+		count++
+		result[row][i] = count
 	}
+
+	for j := 1; j < num; j++ {
+		count++
+		result[j][column] = count
+	}
+
+	for k := num - 2; k >= 0; k-- {
+		count++
+		result[num-1][k] = count
+	}
+
+	return result
 }
