b09eb0c543c59a83752c62c1b1144a4f6ab4715b
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2020-08-02 14:27:21 +0700

    ref(kadane algorithm): refactor function

--- algoexpert/medium/kadane_algorithm/program.go
+++ algoexpert/medium/kadane_algorithm/program.go
@@ -1,6 +1,29 @@
 package program
 
+import (
+	"math"
+)
+
 // KadaneAlgorithm will return maximum of sub array value
+// Time complexity -> O(n ^ 2)
+// Space complexity -> O(1)
 func KadaneAlgorithm(data []int) int {
-	return 1
+	res := math.MinInt8
+
+	for i := 0; i < len(data); i++ {
+		tmpRes := data[i]
+		for j := i + 1; j < len(data); j++ {
+			tmpRes = tmpRes + data[j]
+		}
+		res = max(res, tmpRes)
+	}
+
+	return res
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
 }
--- algoexpert/medium/kadane_algorithm/program_test.go
+++ algoexpert/medium/kadane_algorithm/program_test.go
@@ -17,6 +17,22 @@ func TestKadaneAlgorithm(t *testing.T) {
 			data:   []int{1},
 			result: 1,
 		},
+		"[-2, 1] should return 1": {
+			data:   []int{-2, 1},
+			result: 1,
+		},
+		"[1, 2, 3] should return 6": {
+			data:   []int{1, 2, 3},
+			result: 6,
+		},
+		"[-1,2,3] should return 5": {
+			data:   []int{-1, 2, 3},
+			result: 5,
+		},
+		"[3, 5, -9, 4, 7] should return 11": {
+			data:   []int{3, 5, -9, 4, 7},
+			result: 11,
+		},
 	}
 
 	for testName, test := range tests {
