e37958001848e9f6f743f445f1bc9b73946cdb32
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2020-08-10 06:13:43 +0700

    feat(fibonacci): fibonacci recursive

--- problem/fibonacci/program.go
+++ problem/fibonacci/program.go
@@ -11,3 +11,11 @@ func Fibonacci(num int) int {
 
 	return data[num]
 }
+
+// Fibonacci2 return fibonacci number based on index num using recursive
+func Fibonacci2(num int) int {
+	if num <= 2 {
+		return 1
+	}
+	return Fibonacci2(num-1) + Fibonacci2(num-2)
+}
--- problem/fibonacci/program_test.go
+++ problem/fibonacci/program_test.go
@@ -41,3 +41,37 @@ func TestFibonacci(t *testing.T) {
 		})
 	}
 }
+
+func TestFibonacci2(t *testing.T) {
+	tests := map[string]struct {
+		input  int
+		result int
+	}{
+		"1 should return 1": {
+			input:  1,
+			result: 1,
+		},
+		"2 should return 1": {
+			input:  2,
+			result: 1,
+		},
+		"3 should return 2": {
+			input:  3,
+			result: 2,
+		},
+		"5 should return 5": {
+			input:  5,
+			result: 5,
+		},
+		"6 should return 8": {
+			input:  6,
+			result: 8,
+		},
+	}
+
+	for testName, test := range tests {
+		t.Run(testName, func(t *testing.T) {
+			require.Equal(t, test.result, p.Fibonacci2(test.input))
+		})
+	}
+}
